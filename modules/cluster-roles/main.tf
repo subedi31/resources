00a98cd7a84eb585ffe9a1a030accfcc4a7446c7
subedi31 <amritsubedi3131@gmail.com>
2023-08-10 21:17:58 +0545

    testing namespace and livelinenss

--- modules/cluster-roles/main.tf
+++ modules/cluster-roles/main.tf
@@ -1,16 +1,6 @@
-variable "resource_names" {
-  description = "Names of the resources to apply the role to"
-  type        = list(string)
-  default     = ["admin", "edit", "view"]
+resource "helm_release" "cluster-template" {
+  chart     = ".//modules/namespace/helm-namespace"
+  name      = "cluster-role-chart"
 }
-
-resource "helm_release" "cluster_role_deploy" {
-  chart = "./helm-cluster_role"
-  name  = "cluster-role-chart"
-
-  values = ["./helm-cluster_role/values.yaml"]
-
-}
-
 
 
