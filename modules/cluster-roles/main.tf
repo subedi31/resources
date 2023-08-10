33234f1e89af799966435b18d899c4cf463f6c74
subedi31 <amritsubedi3131@gmail.com>
2023-08-10 21:44:40 +0545

    testing namespace and livelinenss

--- modules/cluster-roles/main.tf
+++ modules/cluster-roles/main.tf
@@ -1,4 +1,4 @@
-resource "helm_release" "cluster-template" {
+resource "helm_release" "cluster-roles-template" {
   chart     = ".//modules/cluster-roles/helm-cluster_role"
   name      = "cluster-role-chart"
 }
