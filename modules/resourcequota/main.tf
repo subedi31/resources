346e4714dc4d1ee16a5f8fb1f59d49b4babaade7
subedi31 <amritsubedi3131@gmail.com>
2023-08-11 14:18:43 +0545

    test quota

--- modules/resourcequota/main.tf
+++ modules/resourcequota/main.tf
@@ -1,4 +1,4 @@
 resource "helm_release" "resourcequota-template" {
-  chart     = ".//modules/resource-quota/helm-resourcequota"
+  chart     = ".//modules/resourcequota/helm-resourcequota"
   name      = "resourcequota-template"
 }
