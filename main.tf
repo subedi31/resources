f746c5bd6fcb93d50ed5e1c948ff4f4ee045c3aa
subedi31 <amritsubedi3131@gmail.com>
2023-09-06 23:01:32 +0545

    testing quota

--- main.tf
+++ main.tf
@@ -17,22 +17,4 @@ module "roles"{
 
 module "resource-quota"{
     source= ".//modules/resourcequota"
-    request_cpu = var.request_cpu
-    requests_memory = var.requests_memory
-    cpu_limit = var.cpu_limit
-    memory_limit = var.memory_limit
-    request_gpu = var.request_gpu
-    configmaps_quota = var.configmaps_quota
-    persistentvolumeclaims = var.persistentvolumeclaims
-    pods = var.pods
-    replicationcontrollers = var.replicationcontrollers
-    secrets = var.secrets
-    services = var.services
-    services_loadbalancers = var.services_loadbalancers
 }
-
-
-
-# module "k8s-gatekeeper" {
-#     source = ".//modules/k8s-gatekeeper" 
-# }
